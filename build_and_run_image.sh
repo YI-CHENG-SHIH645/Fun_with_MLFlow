24027f2eecc056b1add587428334a49017919716
施奕成 <yi-cheng@shiyichengdeMacBook-Pro.local>
2021-05-20 01:38:52 +0800

    add files

--- build_and_run_image.sh
+++ build_and_run_image.sh
@@ -1,3 +1,3 @@
 git pull origin master
 sudo docker build . -t 'fun_with_mlflow'
-sudo docker run --rm -d -p 5050:8888 -v "$PWD":/home/jovyan/work fun_with_mlflow
+sudo docker run --rm -d -p 5050:8888 -v "$PWD":/home/z2x98653322/Fun_with_MLFlow fun_with_mlflow
