6f355bd8a15a3cb1077258a069414e1b098079cd
施奕成 <yi-cheng@shiyichengdeMacBook-Pro.local>
2021-05-20 17:15:52 +0800

    add files

--- build_and_run_image.sh
+++ build_and_run_image.sh
@@ -1,4 +1,4 @@
 git pull origin master
 sudo docker build . -t 'fun_with_mlflow'
-sudo docker run --rm -u jovyan -d -p 5050:8888 -e JUPYTER_ENABLE_LAB=yes \
+sudo docker run --rm -u jovyan -d -p 8888:8888 -p 5050:5000 -e JUPYTER_ENABLE_LAB=yes \
  -v "$PWD:/home/jovyan/work" fun_with_mlflow
